d66abc22cfa2bdbf0b0dc961ec549cd73ebceb90
Kavinkumar Sainathan <kavincoolsout@gmail.com>
2023-08-21 22:58:59 -0700

    Added New Project

---
+++ MapboxSDKTest6/app/src/main/java/com/example/mapboxsdktest6/MainActivity.java
@@ -0,0 +1,50 @@
+package com.example.mapboxsdktest6;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.graphics.Bitmap;
+import android.os.Bundle;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import com.microsoft.appcenter.AppCenter;
+import com.microsoft.appcenter.analytics.Analytics;
+import com.microsoft.appcenter.crashes.Crashes;
+
+public class MainActivity extends AppCompatActivity {
+
+    private WebView mywebView;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        mywebView=(WebView) findViewById(R.id.webview);
+        mywebView.setWebViewClient(new WebViewClient());
+        mywebView.loadUrl("https://mhsmapproject.cocoazi.repl.co");
+        WebSettings webSettings=mywebView.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+
+        //Microsoft AppCenter
+        AppCenter.start(getApplication(), "8a4dd9b3-1eef-4a8b-813c-48b0e21fd80f",
+                Analytics.class, Crashes.class);
+    }
+    public class mywebClient extends WebViewClient {
+        @Override
+        public void onPageStarted(WebView view, String url, Bitmap favicon) {
+            super.onPageStarted(view,url,favicon);
+        }
+        @Override
+        public boolean shouldOverrideUrlLoading(WebView view, String url) {
+            view.loadUrl(url);
+            return true;
+        }
+    }
+    @Override
+    public void onBackPressed() {
+        if (mywebView.canGoBack()) {
+            mywebView.goBack();
+        } else {
+            super.onBackPressed();
+        }
+    }
+}
