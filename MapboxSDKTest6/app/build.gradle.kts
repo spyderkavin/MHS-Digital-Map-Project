a3d50449c2ab5fa30f644b256f637a8df3069e21
Kavinkumar Sainathan <kavincoolsout@gmail.com>
2024-01-31 22:11:29 -0800

    Added new project

--- MapboxSDKTest6/app/build.gradle.kts
+++ MapboxSDKTest6/app/build.gradle.kts
@@ -1,17 +1,21 @@
 plugins {
     id("com.android.application")
+
+    // Add the Google services Gradle plugin
+    id("com.google.gms.google-services")
+
 }
 
 android {
     namespace = "com.example.mapboxsdktest6"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.example.mapboxsdktest6"
-        minSdk = 27
-        targetSdk = 33
+        minSdk = 28
+        targetSdk = 34
         versionCode = 1
-        versionName = "1.0"
+        versionName = "0.5"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
@@ -34,11 +38,29 @@ android {
 dependencies {
 
     implementation("androidx.appcompat:appcompat:1.6.1")
-    implementation("com.google.android.material:material:1.9.0")
+    implementation("com.google.android.material:material:1.10.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
+    implementation("com.google.firebase:firebase-messaging:23.3.1")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
-    implementation ("com.microsoft.appcenter:appcenter-analytics:4.4.5")
-    implementation ("com.microsoft.appcenter:appcenter-crashes:4.4.5")
+
+    //Microsoft AppCenter Dependencies
+    //def appCenterSdkVersion = '4.4.5'
+    implementation("com.microsoft.appcenter:appcenter-analytics:4.4.5")
+    implementation("com.microsoft.appcenter:appcenter-crashes:4.4.5")
+
+    //Google Firebase
+    // Import the Firebase BoM
+    implementation(platform("com.google.firebase:firebase-bom:32.7.1"))
+
+
+    // TODO: Add the dependencies for Firebase products you want to use
+    // When using the BoM, don't specify versions in Firebase dependencies
+    implementation("com.google.firebase:firebase-analytics")
+
+
+    // Add the dependencies for any other desired Firebase products
+    // https://firebase.google.com/docs/android/setup#available-libraries
+
 }
--- MapboxSDKTest6/app/src/main/java/com/example/mapboxsdktest6/MainActivity.java
+++ MapboxSDKTest6/app/src/main/java/com/example/mapboxsdktest6/MainActivity.java
@@ -1,42 +1,80 @@
 package com.example.mapboxsdktest6;
+//maindevrepo
 
-import androidx.appcompat.app.AppCompatActivity;
-
-import android.graphics.Bitmap;
+import android.Manifest;
+import android.app.DownloadManager;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
+import android.os.Environment;
+import android.view.View;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
+import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
+
 import com.microsoft.appcenter.AppCenter;
 import com.microsoft.appcenter.analytics.Analytics;
 import com.microsoft.appcenter.crashes.Crashes;
 
 public class MainActivity extends AppCompatActivity {
-
+    //Variables Declared
     private WebView mywebView;
+    private static final int REQUEST_CODE = 100;
+    public static final String imageURL = "https://th.bing.com/th/id/OIP.4DTFm87FSJeMe_7rydeImAHaE8?rs=1&pid=ImgDetMain";
+    String imageName = "demoImage.png";
+
+    //Code
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        mywebView=(WebView) findViewById(R.id.webview);
+        mywebView = findViewById(R.id.webview);
         mywebView.setWebViewClient(new WebViewClient());
-        mywebView.loadUrl("https://mhsmapproject.cocoazi.repl.co");
-        WebSettings webSettings=mywebView.getSettings();
+        //mywebView.loadUrl("file:///asset/index.htm");
+        WebSettings webSetting = mywebView.getSettings();
+        webSetting.setBuiltInZoomControls(true);
+        mywebView.setWebViewClient(new WebViewClient());
+        mywebView.loadUrl("file:///android_asset/index.html");
+        WebSettings webSettings = mywebView.getSettings();
         webSettings.setJavaScriptEnabled(true);
 
+        // storage runtime permission
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            if (checkSelfPermission(Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
+                requestPermissions(new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE}, REQUEST_CODE);
+            }
+        }
 
+        Button btnDownloadImage = findViewById(R.id.btnDownloadImage);
+        btnDownloadImage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                downloadImage(imageURL, imageName);
+            }
+        });
+
+        //Microsoft App Center
+        AppCenter.start(getApplication(), "8a4dd9b3-1eef-4a8b-813c-48b0e21fd80f",
+                Analytics.class, Crashes.class);
+        Analytics.trackEvent("My custom event");
     }
-    public class mywebClient extends WebViewClient {
-        @Override
-        public void onPageStarted(WebView view, String url, Bitmap favicon) {
-            super.onPageStarted(view,url,favicon);
-        }
-        @Override
-        public boolean shouldOverrideUrlLoading(WebView view, String url) {
-            view.loadUrl(url);
-            return true;
-        }
+    //https://sites.google.com/student.musd.org/dmp/home
+    public void downloadImage(String url, String outputFileName) {
+        DownloadManager.Request request = new DownloadManager.Request(Uri.parse(url));
+        request.setTitle(imageName);
+        request.setDescription("Downloading " + imageName);
+        request.setNotificationVisibility(DownloadManager.Request.VISIBILITY_VISIBLE_NOTIFY_COMPLETED);
+        request.allowScanningByMediaScanner();
+        request.setDestinationInExternalPublicDir(Environment.DIRECTORY_DOWNLOADS, outputFileName);
+        DownloadManager manager = (DownloadManager) getSystemService(Context.DOWNLOAD_SERVICE);
+        manager.enqueue(request);
     }
+
     @Override
     public void onBackPressed() {
         if (mywebView.canGoBack()) {
